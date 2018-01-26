2221d77bb4b593de7fa53050a0708dbb156adbfa
Clay Smith <smithclay@gmail.com>
2018-01-25 19:03:23 -0800

    refactor etherdome libs

--- etherdome/index.js
+++ etherdome/index.js
@@ -1,79 +1,8 @@
-const path = require('path');
-const http = require('http');
 const log = require('lambda-log');
-const { spawn } = require('child_process');
+const jsonRPC = require('./lib/json-rpc');
 
-const GETH_BINARY_NAME = 'geth-linux-amd64-1.7.3-4bb3c89d';
-const GETH_RPC_HOST = 'localhost';
-const GETH_RPC_PORT = 8545;
-const RETRY_TIMEOUT_MS = 500;
-
-const geth = spawn(path.join(process.env.LAMBDA_TASK_ROOT, 'bin', GETH_BINARY_NAME),
-    ['--dev', '--dev.period', '4', '--rpc', '  --ipcdisable']
-);
-
-geth.stdout.on('data', (data) => {
-    log.debug(`geth stdout: ${data}`);
-});
-
-geth.stderr.on('data', (data) => {
-    log.debug(`geth stderr: ${data}`);
-});
-
-geth.on('close', (code) => {
-    log.error(`geth child process exited with code ${code}`);
-});
-
-// Proxy geth JSON-RPC request from gateway
-var proxyRPCRequest = function(requestBody, cb, retries=0) {
-    var opts = {
-        host: GETH_RPC_HOST,
-        port: GETH_RPC_PORT,
-        path: '/',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(requestBody)
-        }
-    };
-
-    var post = http.request(opts, function(res) {
-        res.setEncoding('utf8');
-        var data = '';
-        res.on('data', function (chunk) {
-            data += chunk;
-        });
-        res.on('end', function() {
-            var gatewayResponse = {
-                statusCode: 200,
-                headers: {
-                    'application/type': 'text/json'
-                },
-                body: data
-            };
-            log.info(`json-rpc response ${JSON.stringify(gatewayResponse)}`);
-            cb(null, gatewayResponse);
-        });
-    });
-    post.on('error', function(e) {
-        if (e.code === 'ECONNREFUSED') {
-            // retry
-            setTimeout(function() {
-                retries = retries + 1;
-                log.info('json-rpc connection refused', {retry: retries});
-                proxyRPCRequest(requestBody, cb, retries);
-            }, RETRY_TIMEOUT_MS);
-        } else {
-            log.error(e);
-            cb(null, {
-                statusCode: 500,
-                body: e.toString()
-            });
-        }
-    });
-    post.write(requestBody);
-    post.end();
-}
+// Start jsonRPC in background
+jsonRPC.start();
 
 exports.handler = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
@@ -83,10 +12,19 @@ exports.handler = (event, context, callback) => {
         log.config.debug = true;
     }
 
-    proxyRPCRequest(event.body, function(err, gatewayResponse) {
+    log.debug(JSON.stringify(event));
+
+    jsonRPC.proxyRPCRequest(event.body, function(err, gatewayResponse) {
         if (err) {
             return callback(null, { statusCode: 500, body: err.toString() });
         }
+
+        // If write: save to S3 (or cache?)
+        // admin.exportChain('blockchain_backup')
+        // admin.importChain('blockchain_backup')
+
+        // If read: return immediately
+
         callback(null, gatewayResponse);
     });
 }
---
+++ etherdome/lib/json-rpc.js
@@ -0,0 +1,85 @@
+const log = require('lambda-log');
+const http = require('http');
+const path = require('path');
+
+const { spawn } = require('child_process');
+
+const GETH_BINARY_NAME = 'geth-linux-amd64-1.7.3-4bb3c89d';
+const GETH_RPC_HOST = 'localhost';
+const GETH_RPC_PORT = 8545;
+const RETRY_TIMEOUT_MS = 500;
+
+var proxyRPCRequest = function(requestBody, cb, retries=0) {
+    var opts = {
+        host: GETH_RPC_HOST,
+        port: GETH_RPC_PORT,
+        path: '/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(requestBody)
+        }
+    };
+
+    var post = http.request(opts, function(res) {
+        res.setEncoding('utf8');
+        var data = '';
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function() {
+            var gatewayResponse = {
+                statusCode: 200,
+                headers: {
+                    'application/type': 'text/json'
+                },
+                body: data
+            };
+            log.info(`json-rpc response ${JSON.stringify(gatewayResponse)}`);
+            cb(null, gatewayResponse);
+        });
+    });
+    post.on('error', function(e) {
+        if (e.code === 'ECONNREFUSED') {
+            // retry
+            setTimeout(function() {
+                retries = retries + 1;
+                log.info('json-rpc connection refused', {retry: retries});
+                proxyRPCRequest(requestBody, cb, retries);
+            }, RETRY_TIMEOUT_MS);
+        } else {
+            log.error(e);
+            cb(null, {
+                statusCode: 500,
+                body: e.toString()
+            });
+        }
+    });
+    post.write(requestBody);
+    post.end();
+}
+
+var start = () => {
+    const geth = spawn(path.join(process.env.LAMBDA_TASK_ROOT, 'bin', GETH_BINARY_NAME),
+        ['--dev', '--dev.period', '4', '--rpc', '--ipcdisable']
+    );
+
+    geth.stdout.on('data', (data) => {
+        log.debug(`geth stdout: ${data}`);
+    });
+
+    geth.stderr.on('data', (data) => {
+        log.debug(`geth stderr: ${data}`);
+    });
+
+    geth.on('close', (code) => {
+        log.error(`geth child process exited with code ${code}`);
+    });
+
+    return geth;
+}
+
+module.exports = {
+    proxyRPCRequest: proxyRPCRequest,
+    start: start
+};
